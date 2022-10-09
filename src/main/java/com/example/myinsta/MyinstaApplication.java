dc9e6eddb5915bfc95dbf5fc08e929a66c075ec0
JoowonSuh <sjw2023@naver.com>
2022-10-09 13:14:17 +0900

    first commit

---
+++ src/main/java/com/example/myinsta/MyinstaApplication.java
@@ -0,0 +1,13 @@
+package com.example.myinsta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyinstaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyinstaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myinsta/MyinstaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myinsta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyinstaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
