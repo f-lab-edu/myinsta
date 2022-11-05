f4a3f9ed076cf36b4de7c37511baf1249cc1928c
JoowonSuh <sjw2023@gmail.com>
2022-11-05 20:10:09 +0900

    PR update

--- src/main/java/com/example/myinsta/utils/sql_queries/comment_iamage_table.sql
+++
@@ -1,8 +0,0 @@
-create table comment_image(
-	id_comment_image bigint auto_increment not null primary key,
-    id_comment bigint not null,
-    constraint fk_comment_image_comment foreign key(id_comment) references comment_(id_comment),
-    image_path varchar(4069), -- 파일 경로 linux system 최대 4069 바이트 지원
-    created_date timestamp,
-    updated_date timestamp
-);
--- src/main/java/com/example/myinsta/utils/sql_queries/post_table.sql
+++
@@ -1,8 +0,0 @@
-create table post(
-	id_post bigint auto_increment not null primary key,
-    id_account bigint not null,
-    foreign key(id_account) references account_(id_account),
-    title varchar(150), -- 글 제목 50자 
-    created_date timestamp,
-    updated_date timestamp
-);
