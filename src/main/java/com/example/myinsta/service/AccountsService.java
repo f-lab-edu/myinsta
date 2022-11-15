f727624f071bb033a90060f2a35ed94f19f41a8b
JoowonSuh <sjw2023@gmail.com>
2022-11-15 20:36:03 +0900

    Added selectTest() in Service. Mapped the AccountsService selectTest() with accountsMapper.selectTest()

--- src/main/java/com/example/myinsta/service/AccountsService.java
+++ src/main/java/com/example/myinsta/service/AccountsService.java
@@ -1,6 +1,7 @@
 package com.example.myinsta.service;
 
 import com.example.myinsta.dto.AccountsInsertTestDto;
+import com.example.myinsta.dto.AccountsSelectTestDto;
 import com.example.myinsta.mapper.AccountsMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,5 +14,9 @@ public class AccountsService {
     private final AccountsMapper accountsMapper;
     public int insertTest(AccountsInsertTestDto accountsInsertTestDto){
         return accountsMapper.insertTest( accountsInsertTestDto );
+    }
+
+    public List<AccountsSelectTestDto> selectTest(){
+        return accountsMapper.selectTest();
     }
 }
