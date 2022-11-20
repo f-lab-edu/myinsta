a36150f29e2fa060ae7257ab20873aab12e99df3
JoowonSuh <sjw2023@gmail.com>
2022-11-20 18:18:16 +0900

    change logging level from default to DEBUG to use logger.debug()

--- src/main/java/com/example/myinsta/controller/AccountsController.java
+++ src/main/java/com/example/myinsta/controller/AccountsController.java
@@ -7,15 +7,25 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
+//@RestController : @Controller + @ResponseBody, it response with Json data.
+//@RequiredArgsConstructor : Lombok annotation which makes Constructor with all the member fields as arguments.
+//@Slf4j : To use logger for debugging purpose.
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 public class AccountsController {
 
+    //@RequestMapping : Mapping POST request with "/signUp" url with signUp() endpoint.
     @RequestMapping("/signUp")
-    public int signUp(@RequestBody SignUpDto signUpDto){
-        log.info( signUpDto.getEmail(), signUpDto.getNick_name(), signUpDto.getPassword() );
-        return 1;
+    //@Valid : Using Java Bean Validation to validate the user input
+    //@RequestBody : To bind user input to SignUpDto
+    public String signUp(@RequestBody @Valid SignUpDto signUpDto){
+        //To see what data is being bound to SignUpDto
+        log.debug( "email : {} \n nick_name : {} \n password : {}" ,signUpDto.getEmail() , signUpDto.getNick_name() , signUpDto.getPassword() );
+        //redirect to the root page.
+        return "redirect:/";
     }
 
 }
--- src/main/java/com/example/myinsta/dto/SignUpDto.java
+++ src/main/java/com/example/myinsta/dto/SignUpDto.java
@@ -5,12 +5,16 @@ import lombok.Setter;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
 public class SignUpDto {
+    @Email(message = "invalid Email format", regexp = ( "(.){0,30}@(.){0,20}" ) )
+    @NotNull
     private String email;
+    @NotBlank
     private String nick_name;
+    @NotBlank
     private String password;
 }
