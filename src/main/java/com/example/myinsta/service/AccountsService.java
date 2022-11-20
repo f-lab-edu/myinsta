050362810610e4790c81b9dcb9a85b8539aa67a1
JoowonSuh <sjw2023@gmail.com>
2022-11-20 18:49:00 +0900

    add member field accountsMapper and inject mapper into service implements signUp() using accountsMapper

--- src/main/java/com/example/myinsta/service/AccountsService.java
+++ src/main/java/com/example/myinsta/service/AccountsService.java
@@ -1,6 +1,7 @@
 package com.example.myinsta.service;
 
 import com.example.myinsta.dto.SignUpDto;
+import com.example.myinsta.mapper.AccountsMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,9 +9,10 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class AccountsService {
-
+    //Inject AccountsMapper to query to DB
+    private final AccountsMapper accountsMapper;
     public int signUp(SignUpDto signUpDto){
         //insert accounts information into DB.accounts table
-        return 1;
+        return accountsMapper.insertAccount(signUpDto);
     }
 }
