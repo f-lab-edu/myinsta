f76a77efcba2cae992c163b5aa08ab4a2e367d45
JoowonSuh <sjw2023@gmail.com>
2022-11-20 14:26:42 +0900

    add controller to see what happens with Bean Validation Annotation

--- src/main/java/com/example/myinsta/controller/AccountsController.java
+++ src/main/java/com/example/myinsta/controller/AccountsController.java
@@ -1,6 +1,21 @@
 package com.example.myinsta.controller;
 
+import com.example.myinsta.dto.SignUpDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequiredArgsConstructor
+@Slf4j
 public class AccountsController {
+
+    @RequestMapping("/signUp")
+    public int signUp(@RequestBody SignUpDto signUpDto){
+        log.info( signUpDto.getEmail(), signUpDto.getNick_name(), signUpDto.getPassword() );
+        return 1;
+    }
 
 }
