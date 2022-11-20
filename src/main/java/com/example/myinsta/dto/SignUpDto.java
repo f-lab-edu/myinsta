9b58e3a0666ce7d425f40864b2f522446aa6012a
JoowonSuh <sjw2023@gmail.com>
2022-11-20 12:42:15 +0900

    Adding SignUpDto

---
+++ src/main/java/com/example/myinsta/dto/SignUpDto.java
@@ -0,0 +1,12 @@
+package com.example.myinsta.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SignUpDto {
+    private String email;
+    private String nick_name;
+    private String password;
+}
