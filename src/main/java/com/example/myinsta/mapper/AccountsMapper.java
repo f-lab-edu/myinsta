dff5535cb7e177957b0fd74c7a44a1a9a5696be3
JoowonSuh <sjw2023@gmail.com>
2022-11-30 04:53:56 +0900

    change insertAccount to use AccountsDao

--- src/main/java/com/example/myinsta/mapper/AccountsMapper.java
+++ src/main/java/com/example/myinsta/mapper/AccountsMapper.java
@@ -1,10 +1,11 @@
 package com.example.myinsta.mapper;
 
+import com.example.myinsta.dao.AccountsDao;
 import com.example.myinsta.dto.SignUpDto;
 import org.apache.ibatis.annotations.Mapper;
 
 //@Mapper : To be scanned by mapper-scanner of mybatis.
 @Mapper
 public interface AccountsMapper {
-    int insertAccount(SignUpDto signUpDto);
+    int insertAccount(AccountsDao accountsDao);
 }
