2dabe8ed73cc09e07cd66dcab25203219ba44c46
JoowonSuh <sjw2023@gmail.com>
2022-12-13 19:27:06 +0900

    Revert "feature-8: add post update service"
    This reverts commit 12a4e3b9ab6b7e88333df84e4ac3ba4f8bd1564b.

--- src/main/java/com/example/myinsta/controller/PostsController.java
+++ src/main/java/com/example/myinsta/controller/PostsController.java
@@ -1,17 +1,16 @@
 package com.example.myinsta.controller;
 
 import com.example.myinsta.dto.PostCreateDto;
-import com.example.myinsta.dto.PostUpdateDto;
-import com.example.myinsta.exception.CustomException;
-import com.example.myinsta.exception.ErrorCode;
 import com.example.myinsta.service.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,10 +21,5 @@ public class PostsController {
     public ResponseEntity<Void> postCreation(@Valid @RequestBody PostCreateDto postCreateDto){
         postService.postCreation(postCreateDto);
         return new ResponseEntity<> (HttpStatus.CREATED);
-    }
-    @PatchMapping("/{id}")
-    public ResponseEntity<Void> postUpdate(@PathVariable Long id, @Valid @RequestBody PostUpdateDto postUpdateDto){
-        postService.postUpdate(postUpdateDto, id);
-        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- src/main/java/com/example/myinsta/service/PostsService.java
+++ src/main/java/com/example/myinsta/service/PostsService.java
@@ -1,11 +1,8 @@
 package com.example.myinsta.service;
 
 import com.example.myinsta.dao.PostImageDao;
-import com.example.myinsta.dao.PostImagesUpdateDao;
 import com.example.myinsta.dao.PostsDao;
-import com.example.myinsta.dao.PostsUpdateDao;
 import com.example.myinsta.dto.PostCreateDto;
-import com.example.myinsta.dto.PostUpdateDto;
 import com.example.myinsta.exception.CustomException;
 import com.example.myinsta.exception.ErrorCode;
 import com.example.myinsta.mapper.PostsMapper;
@@ -35,29 +32,7 @@ public class PostsService {
                 .build();
         result = postsMapper.insertPostImage(postImageDao);
         if(result != 1){
-            throw new CustomException(ErrorCode.FAILED_TO_INSERT_POST_IMAGE);
-        }
-    }
-
-    public void postUpdate(PostUpdateDto postUpdateDto, Long id){
-        PostsUpdateDao postsUpdateDao = PostsUpdateDao.builder()
-                .idPost(id)
-                .title(postUpdateDto.getTitle())
-                .build();
-
-        int result = postsMapper.updatePost(postsUpdateDao);
-        if(result != 1){
-            throw new CustomException(ErrorCode.FAILED_TO_UPDATE_POST);
-        }
-
-        PostImagesUpdateDao postImagesUpdateDao = PostImagesUpdateDao.builder()
-                .idPost(id)
-                .imagePath(postUpdateDto.getImageUrl())
-                .build();
-
-        result = postsMapper.updatePostImage(postImagesUpdateDao);
-        if(result != 1){
-            throw new CustomException(ErrorCode.FAILED_TO_UPDATE_POST_IMAGE);
+            throw new CustomException(ErrorCode.FAILED_TO_INSERT_POST);
         }
     }
 }
